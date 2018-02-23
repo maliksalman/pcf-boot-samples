27498a352eff2139515089d243c0f05b45da624e
Salman Malik <smalik@pivotal.io>
2018-02-22 22:36:20 -0600

    Renamed/reformatted a bunch of files

--- the-basics/ci/tasks/build-and-test.sh
+++ the-basics/ci/tasks/build-and-test.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 mvn -f the-basics-repo/the-basics/pom.xml package
-
 mkdir -p the-basics-output/target
 cp the-basics-repo/the-basics/target/*.jar the-basics-output/target/
 cp the-basics-repo/the-basics/manifest.yml the-basics-output/
-
-find /tmp/
