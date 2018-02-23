c3c01f819710a76066661b426d78f9905d44f93f
Salman Malik <smalik@pivotal.io>
2018-02-22 22:41:22 -0600

    adding an echo statement

--- the-basics/ci/tasks/build-and-test.sh
+++ the-basics/ci/tasks/build-and-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Now Running in a script!!!"
 mvn -f the-basics-repo/the-basics/pom.xml package
 mkdir -p the-basics-output/target
 cp the-basics-repo/the-basics/target/*.jar the-basics-output/target/
