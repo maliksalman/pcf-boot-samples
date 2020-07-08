06465401ed4d85a2ee451a0e30fd3d1a9e75ad3d
Salman Malik <smalik@pivotal.io>
2020-07-08 16:35:54 -0400

    Updating simple-router to finchley.sr4 and emit prometheus metrics

--- simple-router/src/main/java/com/smalik/SimpleRouterApplication.java
+++ simple-router/src/main/java/com/smalik/SimpleRouterApplication.java
@@ -1,8 +1,11 @@
 package com.smalik;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,5 +13,15 @@ public class SimpleRouterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleRouterApplication.class, args);
+	}
+
+	@Bean
+	public MeterRegistryCustomizer<MeterRegistry> commonTagsRegistryCustomizer() {
+		return new MeterRegistryCustomizer<MeterRegistry>() {
+			@Override
+			public void customize(MeterRegistry registry) {
+				registry.config().commonTags("application", "simple-router");
+			}
+		};
 	}
 }
